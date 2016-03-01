d84fd39d96487ffd2a23292ec0351a273565b14e
Sebastian Weichwald <git@sweichwald.de>
2016-03-01 10:56:05 +0100

    Implement optional argument C for MERLiN.m.

--- matlab/MERLiN.m
+++ matlab/MERLiN.m
@@ -3,16 +3,23 @@
 %      S: (m x 1) vector of samples of S
 %      F: (d x m) matrix of linear mixture samples
 %      v: (d x 1) vector corresponding to C1 in S->C1
+%  Optional input
+%      'C', C: precomputed samples of middle node, v will be ignored
 %  Output
 %      w: found (d x 1) vector
 %      converged: whether the stopping criterion of Stiefel gradient ascent was met
 %      curob: value of f at w
-function [w, converged, curob] = MERLiN(S,F,v)
+function [w, converged, curob] = MERLiN(S,F,v,varargin)
 
 [d, m] = size(F);
 
 %  set C
-C = F'*v;
+%  optional C given?
+if ~isempty(find(strcmp(varargin,'C')))
+    C = varargin{find(strcmp(varargin,'C'))+1};
+else
+    C = F'*v;
+end
 
 %  remove v from F
 F = null(v')'*F;
